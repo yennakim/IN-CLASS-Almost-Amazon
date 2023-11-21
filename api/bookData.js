814be78d897cfafd53e4f8a024983ae92d12d201
Daun Kim <yennakim11@gmail.com>
2023-11-21 17:25:07 -0600

    filtered books on sale

--- api/bookData.js
+++ api/bookData.js
@@ -29,7 +29,17 @@ const createBook = () => {};
 const updateBook = () => {};
 
 // TODO: FILTER BOOKS ON SALE
-const booksOnSale = () => {};
+const booksOnSale = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books.json?orderBy="sale"&equalTo=true`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 // TODO: STRETCH...SEARCH BOOKS
 
--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,5 +1,5 @@
 import { signOut } from '../utils/auth';
-import { getBooks } from '../api/bookData';
+import { booksOnSale, getBooks } from '../api/bookData';
 import { showBooks } from '../pages/books';
 
 // navigation events
@@ -10,7 +10,7 @@ const navigationEvents = () => {
 
   // TODO: BOOKS ON SALE
   document.querySelector('#sale-books').addEventListener('click', () => {
-    console.warn('CLICKED SALE BOOKS');
+    booksOnSale().then(showBooks);
   });
 
   // TODO: ALL BOOKS

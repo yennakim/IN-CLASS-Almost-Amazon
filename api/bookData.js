5f83651d0ecd427bc677744b73fbabcff88c50b6
Daun Kim <yennakim11@gmail.com>
2023-11-21 16:57:27 -0600

    get all books on DOM

--- api/bookData.js
+++ api/bookData.js
@@ -1,10 +1,20 @@
-// import client from '../utils/client';
+import client from '../utils/client';
 // API CALLS FOR BOOKS
 
-// const endpoint = client.databaseURL;
+const endpoint = client.databaseURL;
 
 // TODO: GET BOOKS
-const getBooks = () => {};
+const getBooks = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 // TODO: DELETE BOOK
 const deleteBook = () => {};
--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,4 +1,6 @@
 import { signOut } from '../utils/auth';
+import { getBooks } from '../api/bookData';
+import { showBooks } from '../pages/books';
 
 // navigation events
 const navigationEvents = () => {
@@ -13,7 +15,7 @@ const navigationEvents = () => {
 
   // TODO: ALL BOOKS
   document.querySelector('#all-books').addEventListener('click', () => {
-    console.warn('CLICKED ALL BOOKS');
+    getBooks().then(showBooks);
   });
 
   // FIXME: STUDENTS Create an event listener for the Authors

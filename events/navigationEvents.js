dc6ad1a44a2f659d5c7f9fda4b2a1e01944b78ea
Daun Kim <yennakim11@gmail.com>
2023-11-21 20:28:25 -0600

    authors are rendering when clicking on Authors navbar button

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,6 +1,8 @@
 import { signOut } from '../utils/auth';
 import { booksOnSale, getBooks } from '../api/bookData';
 import { showBooks } from '../pages/books';
+import { getAuthors } from '../api/authorData';
+import { showAuthors } from '../pages/authors';
 
 // navigation events
 const navigationEvents = () => {
@@ -23,7 +25,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    console.warn('CLICKED AUTHORS');
+    getAuthors().then(showAuthors);
   });
 
   // STRETCH: SEARCH

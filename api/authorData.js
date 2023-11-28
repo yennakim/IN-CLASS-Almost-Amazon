c84f02e39535ec7fc2d3a94d5ac54bb9742c6f32
Daun Kim <yennakim11@gmail.com>
2023-11-28 16:46:44 -0600

    created get single author books Promise, still need to do mergedData for authors

--- api/authorData.js
+++ api/authorData.js
@@ -84,7 +84,17 @@ const updateAuthor = (payload) => new Promise((resolve, reject) => {
 });
 
 // TODO: GET A SINGLE AUTHOR'S BOOKS
-const getAuthorBooks = () => {};
+const getAuthorBooks = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books.json?orderBy="author_id"&equalTo=${firebaseKey}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
 export {
   getAuthors,

f33fac8579fafc875924c49f221c1b81b20397d9
Daun Kim <yennakim11@gmail.com>
2023-11-28 16:29:40 -0600

    added book detail functionality

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,16 @@
 // for merged promises
+
+import { getSingleAuthor } from './authorData';
+import { getSingleBook } from './bookData';
+
+// TODO: Get data for viewBook
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleBook(firebaseKey).then((bookObject) => {
+    getSingleAuthor(bookObject.author_id)
+      .then((authorObject) => resolve({ ...bookObject, authorObject }));
+  }).catch(reject);
+// GET AUTHOR
+// Create an object that has book data and an object named authorObject
+});
+
+export default getBookDetails;

a12ee1d5accfb7d5b2aa171293210a680266ec74
Daun Kim <yennakim11@gmail.com>
2023-11-29 17:34:00 -0600

    added author view

--- api/authorData.js
+++ api/authorData.js
@@ -85,7 +85,7 @@ const updateAuthor = (payload) => new Promise((resolve, reject) => {
 
 // TODO: GET A SINGLE AUTHOR'S BOOKS
 const getAuthorBooks = (firebaseKey) => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/books.json?orderBy="author_id"&equalTo=${firebaseKey}`, {
+  fetch(`${endpoint}/books.json?orderBy="author_id"&equalTo="${firebaseKey}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
